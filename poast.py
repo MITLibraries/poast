14ceb1168fcad5d648ea298f64a1d88834d2846a
Mike Graves <mgraves@mit.edu>
2015-03-16 12:04:56 -0400

    Add mail command

--- poast.py
+++ poast.py
@@ -4,8 +4,9 @@ from __future__ import absolute_import
 import click
 from tempfile import NamedTemporaryFile
 from email.generator import Generator
+import smtplib
 
-from poast import DATE_TYPE, message_queue
+from poast import DATE_TYPE, message_queue, delivery_queue
 from poast.config import Config
 
 
@@ -23,6 +24,19 @@ def queue(start, end, dir, cfg_var="POAST_CONFIG"):
     for msg in message_queue(start, end, cfg):
         with NamedTemporaryFile(dir=dir, delete=False, prefix='') as fp:
             Generator(fp).flatten(msg)
+
+
+@cli.command()
+@click.argument('path', type=click.Path(exists=True))
+@click.confirmation_option(prompt='Are you sure you want to send emails?')
+def mail(path, cfg_var="POAST_CONFIG"):
+    cfg = Config.from_envvar(cfg_var)
+    s = smtplib.SMTP(cfg['SMTP_HOST'])
+    for msg in delivery_queue(path):
+        receiver = msg['To']
+        sender = msg['From']
+        s.sendmail(sender, receiver, msg.as_string())
+    s.quit()
 
 
 if __name__ == '__main__':
